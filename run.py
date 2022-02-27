ca8d424076e407c223e56319632f675111c3e14c
Sorin05 <sorinandrei230587@gmail.com>
2022-02-27 10:09:06 +0000

    Create a  function that returns a random card from the deck

--- run.py
+++ run.py
@@ -1,3 +1,13 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+from art import logo
+
+
+
+def deal_card():
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    card = random.choice(cards)
+    return card
+
+
+
+
