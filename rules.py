2e99b65d961369f876a41a8be75e687979487ab3
Sorin05 <sorinandrei230587@gmail.com>
2022-03-08 11:07:29 +0000

    Add blackjack rules

---
+++ rules.py
@@ -0,0 +1,15 @@
+""" BlackJack Rules"""
+
+
+def print_rules():
+    """ Prints the rules"""
+
+    print("\nThe deck is unlimmited in size.")
+    print("\nThere are no jokers.")
+    print("\nThe Jack/Queen/King all count as 10.")
+    print("\nThe the Ace can count as 11 or 1.")
+    print("\nUse the following list as the deck of cards:")
+    print("\n Use the following list as the deck of cards:")
+    print("cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]")
+    print("\nThe cards in the list have equal probability of being drawn.")
+    print("\n Cards are not removed from the deck as they are drawn.")
