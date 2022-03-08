62d8f5af8635b1de089acbee43973ad37d577ad1
Sorin05 <sorinandrei230587@gmail.com>
2022-03-08 12:58:45 +0000

    Fix identation error

--- rules.py
+++ rules.py
@@ -1,15 +1,10 @@
-""" BlackJack Rules"""
+RULES = """############### Our Blackjack House Rules #####################
 
-
-def print_rules():
-    """ Prints the rules"""
-
-    print("\nThe deck is unlimmited in size.")
-    print("\nThere are no jokers.")
-    print("\nThe Jack/Queen/King all count as 10.")
-    print("\nThe the Ace can count as 11 or 1.")
-    print("\nUse the following list as the deck of cards:")
-    print("\n Use the following list as the deck of cards:")
-    print("cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]")
-    print("\nThe cards in the list have equal probability of being drawn.")
-    print("\n Cards are not removed from the deck as they are drawn.")
+## The deck is unlimited in size.
+## There are no jokers.
+## The Jack/Queen/King all count as 10.
+## The the Ace can count as 11 or 1.
+## Use the following list as the deck of cards:
+## cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+## The cards in the list have equal probability of being drawn.
+## Cards are not removed from the deck as they are drawn."""
--- run.py
+++ run.py
@@ -69,27 +69,27 @@ def play_game():
         print(f" Your Cards: {user_cards}, current score: {user_score}")
         print(f" Computer's first card: {computer_cards[0]}")
 
-    if user_score == 0 or computer_score == 0 or user_score > 21:
-        is_game_over = True
-    else:
-        user_should_deal = input(
-            "Type 'y' to get another card, type 'n' to pass: "
-        )
-        if user_should_deal == "y":
-            user_cards.append(deal_card())
-        else:
+        if user_score == 0 or computer_score == 0 or user_score > 21:
             is_game_over = True
+        else:
+            user_should_deal = input(
+                "Type 'y' to get another card, type 'n' to pass: "
+            )
+            if user_should_deal == "y":
+                user_cards.append(deal_card())
+            else:
+                is_game_over = True
 
-    while computer_score != 0 and computer_score < 17:
-        computer_cards.append(deal_card())
-        computer_score = calculate_score(computer_cards)
+        while computer_score != 0 and computer_score < 17:
+            computer_cards.append(deal_card())
+            computer_score = calculate_score(computer_cards)
 
-    print(f"  Your final hand: {user_cards}, final score: {user_score}")
-    print(
-        f"  Computer's final hand: {computer_cards}, "
-        "final score: {computer_score}"
-    )
-    print(compare(user_score, computer_score))
+        print(f"  Your final hand: {user_cards}, final score: {user_score}")
+        print(
+            f"  Computer's final hand: {computer_cards}, "
+            "final score: {computer_score}"
+        )
+        print(compare(user_score, computer_score))
 
 
 while input(
